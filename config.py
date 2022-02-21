7b11dc2f0bea98fbb742fb621fab7026f66d8175
ritik081 <ritk081@gmail.com>
2022-02-22 00:50:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "11964337"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "8ab5ee2b2620a9609048ef426d4861df" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5297403482:AAHKrbfuOgZxnVhccuOYsin9bxhiXT2mboo" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
